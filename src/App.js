9b8c1e53e9292c3a55a4d572584a6e644edd5ede
bhlynskyExadel <bhlynskyi@exadel.com>
2021-07-05 15:08:06 +0300

    Added makeStyles intead .scss in Header. Override primary palette color

--- src/App.js
+++ src/App.js
@@ -4,24 +4,34 @@ import { Route, Switch, HashRouter } from 'react-router-dom';
 import AdminPage from './pages/AdminPage';
 import MainPage from './pages/MainPage';
 import UserPage from './pages/UserPage';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#a71c1b',
+    },
+  },
+});
 
 function App() {
   return (
-    <HashRouter>
-      <Header />
+    <ThemeProvider theme={theme}>
+      <HashRouter>
+        <Header />
 
-      <Switch>
-        <Route path="/main">
-          <MainPage />
-        </Route>
-        <Route path="/user">
-          <UserPage />
-        </Route>
-        <Route path="/admin">
-          <AdminPage />
-        </Route>
-      </Switch>
-    </HashRouter>
+        <Switch>
+          <Route path="/main">
+            <MainPage />
+          </Route>
+          <Route path="/user">
+            <UserPage />
+          </Route>
+          <Route path="/admin">
+            <AdminPage />
+          </Route>
+        </Switch>
+      </HashRouter>
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@material-ui/core/styles';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
