c17a11be69842c3aefd6818c2ad2973a3bea2f9d
bhlynskyExadel <bhlynskyi@exadel.com>
2021-07-06 11:50:01 +0300

    add empty lines
    added comment so git will know that files was changed

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,4 +1,7 @@
 import React from 'react';
+
 export default function MainPage() {
+  //main page
+
   return <h2>Main page. Public</h2>;
 }
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,4 +1,7 @@
 import React from 'react';
+
 export default function UserPage() {
+  //this is user page where he can change settings and info
+
   return <h2>This is user page. </h2>;
 }
