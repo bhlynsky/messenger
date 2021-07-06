2044596044e1a37b7e63b7ada655f8db4f51bfa5
bhlynskyExadel <bhlynskyi@exadel.com>
2021-07-06 15:46:24 +0300

    [2] Compare Objects with nested objects and array
    Changed function for comparing objects. Using recursion if nested object detected.
    Also added tests for all cases(in my opinion).

--- src/pages/TaskPage.js
+++ src/pages/TaskPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useState } from 'react';
 import compareNestedObjects from '../tasks/compareNestedObjects';
 
 export default function TaskPage() {
@@ -22,9 +21,9 @@ export default function TaskPage() {
     color: 'white',
     owner: {
       firstName: 'James',
-      lastName: 'Doe',
-      age: 50,
-      eyeColor: 'blue',
+      lastName: 'damn',
+      age: 533131,
+      eyeColor: 'dwadawdwadawdawdwadawd',
     },
   };
   const randomObj = {
@@ -33,39 +32,29 @@ export default function TaskPage() {
     position: 'Junior Developer',
     interests: { pets: ['cat', 'dog'], hobbies: ['guitar', 'video games'] },
   };
-  const randomObj2 = {
+
+  const randomObj3 = {
     name: 'Boris',
+    interests: { pets: ['cat', 'dog'], hobbies: ['guitar', 'video games'] },
     surname: 'Hlynsky',
     position: 'Junior Developer',
-    interests: { pets: ['cat', 'dog'], hobbies: ['guitar', 'video games'] },
   };
 
   return (
     <div>
       <h1>Compare nested objects</h1>
-      {compareNestedObjects(car, carCopy) ? <h3>Identical objects</h3> : <h3>Different objects</h3>}
 
-      {compareNestedObjects(car, otherCar) ? (
-        <h3>Identical objects</h3>
-      ) : (
-        <h3>Different objects</h3>
-      )}
+      <h2>Identical Objects test</h2>
+      {compareNestedObjects(car, carCopy) ? <h3>Passed</h3> : <h3>Failed</h3>}
+      <h2>Different Objects test</h2>
 
-      {compareNestedObjects(carCopy, otherCar) ? (
-        <h3>Identical objects</h3>
-      ) : (
-        <h3>Different objects</h3>
-      )}
-      {compareNestedObjects(randomObj, otherCar) ? (
-        <h3>Identical objects</h3>
-      ) : (
-        <h3>Different objects</h3>
-      )}
-      {compareNestedObjects(randomObj, randomObj2) ? (
-        <h3>Identical objects</h3>
-      ) : (
-        <h3>Different objects</h3>
-      )}
+      {compareNestedObjects(car, otherCar) ? <h3>Failed</h3> : <h3> Passed</h3>}
+      <h2>Completely different objects</h2>
+
+      {compareNestedObjects(randomObj, otherCar) ? <h3>Failed</h3> : <h3>Passed</h3>}
+      <h2>Identical Objects with nested arrays and random key order</h2>
+
+      {compareNestedObjects(randomObj, randomObj3) ? <h3>Passed</h3> : <h3>Failed</h3>}
     </div>
   );
 }
--- src/tasks/compareNestedObjects.js
+++ src/tasks/compareNestedObjects.js
@@ -1,6 +1,46 @@
+let isEqual;
 export default function compareNestedObjects(obj1, obj2) {
-  console.log(obj1);
-  console.log(obj2);
+  isEqual = true;
 
-  return JSON.stringify(obj1) === JSON.stringify(obj2);
+  let keys1 = Object.keys(obj1);
+  let keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) return false;
+
+  for (let key of keys1) {
+    let value1 = obj1[key];
+    let value2 = obj2[key];
+
+    console.log('curr value 1: ' + value1);
+    console.log('curr value 2: ' + value2);
+
+    if (Array.isArray(value1)) {
+      compareArrays(value1, value2);
+    } else if (typeof value1 !== 'object' || typeof value2 !== 'object') {
+      if (value1 !== value2) {
+        isEqual = false;
+        break;
+      }
+    } else if (typeof value1 === 'object' || typeof value2 === 'object')
+      compareNestedObjects(value1, value2); // recursion for nested objects
+  }
+  console.log(isEqual);
+  return isEqual;
 }
+
+const compareArrays = (a, b) => {
+  if (a.length === b.length) {
+    a.sort();
+    b.sort();
+
+    a.forEach((el, i) => {
+      if (el !== b[i]) {
+        isEqual = false;
+        return;
+      }
+    });
+  } else {
+    isEqual = false;
+    return;
+  }
+};
