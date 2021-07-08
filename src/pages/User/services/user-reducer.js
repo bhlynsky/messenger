1b7f53d539f11f42c30ff4845d3d523ca28af55d
bhlynskyExadel <bhlynskyi@exadel.com>
2021-07-08 15:56:59 +0300

    Update user-reducer.js
    empty lines

--- src/pages/User/services/user-reducer.js
+++ src/pages/User/services/user-reducer.js
@@ -1,10 +1,11 @@
 import { createStore } from 'redux';
 import { initialState } from './user-services';
+
 const userActions = {};
 userActions.actionsType = {
   CHANGE_DATA: 'CHANGE DATA',
 };
-//userActions.
+
 function userReducer(state = initialState, action) {
   switch (action.type) {
     case userActions.actionsType.CHANGE_DATA:
