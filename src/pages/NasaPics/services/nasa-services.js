271de74e57a583582b7dc85dd56e895411ac7380
bhlynskyExadel <bhlynskyi@exadel.com>
2021-07-15 14:33:44 +0300

    update status code validation

--- src/pages/NasaPics/services/nasa-services.js
+++ src/pages/NasaPics/services/nasa-services.js
@@ -7,7 +7,7 @@ const getData = () => (dispatch) => {
     fetch(fetchUrl)
         .then((res) => res.json())
         .then((res) => {
-            if (res.code) throw new Error('something wrong');
+            if (res.code && (res.code > 300 || res < 200)) throw new Error('something wrong');
             else {
                 dispatch(setData(res));
             }
