1d9c814198dc00ffc088745b4ec9314434f590fc
bhlynskyExadel <bhlynskyi@exadel.com>
2021-07-21 13:01:59 +0300

    Update MainPage.js

--- src/pages/Main/MainPage.js
+++ src/pages/Main/MainPage.js
@@ -73,7 +73,6 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => ({
     loadData: (data) => dispatch(loadData(data)),
-    changeGroup: (otherGroup) => dispatch(changeCurrentGroup(otherGroup)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
